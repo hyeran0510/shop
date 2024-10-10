5a422146a989b3214fdd6cf72ccb32d828732d00
hyeranpakr <hr9151@naver.com>
2024-10-10 11:06:52 +0900

    로그인 리다이렉트 수정

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -4,8 +4,6 @@ import com.shop.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,36 +14,35 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
     @Autowired
     MemberService memberService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(authorizeHttpRequestsCustomizer -> authorizeHttpRequestsCustomizer
-                        .requestMatchers("/css/**", "/js/**", "/img/**", "/images/**").permitAll()
-                        .requestMatchers("/", "/members/**", "/item/**", "/images/**", "/bucket/**").permitAll()
-                        .requestMatchers("/question/list").permitAll() // 자유게시판 목록 접근 허용
-// 버킷리스트 경로 추가
+                        .requestMatchers("/css/**", "/js/**", "/img/**").permitAll()
+                        .requestMatchers("/", "/members/**", "/item/**", "/images/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/members/login").permitAll() // 로그인 페이지 접근 허용
                         .anyRequest()
                         .authenticated()
                 ).formLogin(formLoginCustomizer -> formLoginCustomizer
                         .loginPage("/members/login")
-                        .defaultSuccessUrl("/")
+                        .defaultSuccessUrl("/bucket/list")
                         .usernameParameter("email")
                         .failureUrl("/members/login/error")
                         .failureHandler(new CustomAuthenticationFailureHandler())
-                ).logout(logoutCustomizer -> logoutCustomizer
+                ).logout( logoutCustomizer -> logoutCustomizer
                         .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
                         .logoutSuccessUrl("/")
+
                 )
-                .build();
+                .build()
+                ;
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
